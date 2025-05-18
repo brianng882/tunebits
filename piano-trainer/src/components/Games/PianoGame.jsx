f84a0b90fedb879de5efe7e65d22aa63ff4a2075
Brian Chun Yin Ng <chun.yin.ng.24@dartmouth.edu>
2025-05-18 01:26:30 -0400

    Make changes

--- piano-trainer/src/components/Games/PianoGame.jsx
+++ piano-trainer/src/components/Games/PianoGame.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, useRef, useCallback } from 'react';
+import React, { useState, useEffect, useCallback, useRef } from 'react';
 import * as Tone from 'tone';
 import ClimbingGame from './ClimbingGame';
 
